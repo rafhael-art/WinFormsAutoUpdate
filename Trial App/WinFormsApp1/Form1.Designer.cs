20d9ec724be07a6ba9f03b22a3d02afce17a3966
RafhaelPP <rafhaelpillaca2001@gmail.com>
2023-02-06 10:08:05 -0500

    5

--- Trial App/WinFormsApp1/Form1.Designer.cs
+++ Trial App/WinFormsApp1/Form1.Designer.cs
@@ -291,7 +291,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.metroLabel1);
             this.Name = "Form1";
-            this.Text = "Formulario Prueba 4";
+            this.Text = "Formulario Prueba 5";
             ((System.ComponentModel.ISupportInitialize)(this.metroGrid1)).EndInit();
             this.metroContextMenu1.ResumeLayout(false);
             this.ResumeLayout(false);
